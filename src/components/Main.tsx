0368d30a9702862c62ded7351caa9975b272291c
Ivan Bowen <bowenivan16@gmail.com>
2023-01-13 22:52:11 +0300

    add style

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,16 @@
 import React from "react"
+import ReactLogo from "../assets/react_gray.svg"
+
 import "./Main.css"
 
 const Main = () => {
    return (
-      <div className="main">
+      <div className="main" style={{
+         backgroundImage: `url(${ReactLogo})`,
+         backgroundRepeat: "no-repeat",
+         backgroundSize: "70% auto",
+         backgroundPosition: "right -70% bottom 60%"
+      }}>
          <h1>Fun facts about React</h1>
          <ul className="main__ul">
             <li>Was first released in 2013</li>
